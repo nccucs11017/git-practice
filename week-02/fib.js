fad201380ca49a8afc0c9bc22e9cc63dab2b9c99
nccucs11017 <101815460+nccucs11017@users.noreply.github.com>
2024-09-30 11:30:42 +0800

    feat: week-02/fib.js/fib3 better dp

--- week-02/fib.js
+++ week-02/fib.js
@@ -15,6 +15,15 @@ function fib2(n) {
   return arr[n];
 }
 
+function fib3(n) {
+  // better dp, 9/30 update
+  let arr = [0, 1];
+  for (let i = 2; i <= n; ++i) {
+    arr[i&1] = arr[i&1] + arr[(~i)&1];
+  }
+  return arr[n&1];
+}
+
 const fibonacci = fib2; // 所以我的函式應該要叫做 fib(n) 還是 fibonacci(n)？
 
 console.log("fib(0) = " + fib(0));
@@ -22,3 +31,4 @@ console.log(`fib(1) = ${fib(1)}`);
 console.log("fib(5) = " + fib(5).toString());
 console.log("fib(10) = " + fib(10));
 console.log("fibonacci(50) = " + fibonacci(50));
+console.log("fib3(75) = " + fib3(75));
